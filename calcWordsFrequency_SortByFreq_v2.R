a6764ab98c81076bf12e38c50d72b86f69c791e8
todd zhang <phray.zhang@gmail.com>
2016-12-18 22:09:19 +0800

    daily commit

---
+++ calcWordsFrequency_SortByFreq_v2.R
@@ -0,0 +1,12 @@
+findwords<-function(tf){
+    # read in the words 
+    txt<-scan(tf,"")
+    words<-split(1:length(txt),txt) 
+    # the 2nd parameter is factors
+    return(words)
+}
+
+thedata<-findwords("~/dev/git/paRLang/raw.txt")
+# freqData<-freqwl(thedata)
+# lthData<-length(freqData)
+# barplot(freqData[round(0.9*lthData):lthData])
--- factorCalcIncome.R
+++ factorCalcIncome.R
@@ -1,3 +1,6 @@
 d<-data.frame(list(gender=c("M","M","F","M","F","F"),age=c(47,59,31,21,33,24),income=c(55000,88000,76500,12300,45650,77500)))
 d$over25<-ifelse(d$age>25,1,0)
 tapply(d$income,list(d$gender,d$over25),mean)
+
+# the second approach
+split(d$income,list(d$gender,d$over25))
